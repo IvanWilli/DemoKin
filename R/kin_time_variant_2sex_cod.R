725e1736328c540ccef52ba16e9c545d07ae443b
Benjamin-Samuel Schlüter <benjamin-samuel.schluter@uclouvain.be>
2024-02-20 13:04:02 -0500

    script to compare functions with/without cod

--- R/kin_time_variant_2sex_cod.R
+++ R/kin_time_variant_2sex_cod.R
@@ -24,44 +24,44 @@
 
 ## BEN: ========================================================================
 # Function building:
-library(DemoKin)
-library(tidyr)
-library(dplyr)
-library(here)
-
-# Input of model
-years <- ncol(swe_px)
-ages <- nrow(swe_px)
-ff <- swe_asfr
-fm <- rbind(matrix(0, 5, years),
-            swe_asfr[-((ages-4):ages),]) * 1.05
-pf <- swe_px
-pm <- swe_px ^ 1.5
-
-sex_focal = "f"
-time_invariant = FALSE
-birth_female = .5
-output_cohort = 1900 # like in the vignette
-output_period = NULL
-output_kin = NULL
-
-
-pif <- pim <- NULL
-nf <- nm <- NULL
-list_output = FALSE
-
-# Create a fictitious hazard matrix with three causes of death, where each
-# year is a list item (Hazard matrix needs to be (causes * ages) for the
-# matrix algebra to work well with existing code).
-H <- matrix(c(0.5, 1, 2), nrow = 3, ncol = nrow(pf))
-Hf <- Hm <- sapply(colnames(pf), function(x) {
-  return(H)
-  },
-  simplify = FALSE,
-  USE.NAMES = TRUE
-  )
-# BEN: Load time invariant for COD
-source("./R/kin_time_invariant_2sex_cod.R")
+# library(DemoKin)
+# library(tidyr)
+# library(dplyr)
+# library(here)
+#
+# # Input of model
+# years <- ncol(swe_px)
+# ages <- nrow(swe_px)
+# ff <- swe_asfr
+# fm <- rbind(matrix(0, 5, years),
+#             swe_asfr[-((ages-4):ages),]) * 1.05
+# pf <- swe_px
+# pm <- swe_px ^ 1.5
+#
+# sex_focal = "f"
+# time_invariant = FALSE
+# birth_female = .5
+# output_cohort = 1900 # like in the vignette
+# output_period = NULL
+# output_kin = NULL
+#
+#
+# pif <- pim <- NULL
+# nf <- nm <- NULL
+# list_output = FALSE
+#
+# # Create a fictitious hazard matrix with three causes of death, where each
+# # year is a list item (Hazard matrix needs to be (causes * ages) for the
+# # matrix algebra to work well with existing code).
+# H <- matrix(c(0.5, 1, 2), nrow = 3, ncol = nrow(pf))
+# Hf <- Hm <- sapply(colnames(pf), function(x) {
+#   return(H)
+#   },
+#   simplify = FALSE,
+#   USE.NAMES = TRUE
+#   )
+# # BEN: Load time invariant for COD
+# source("./R/kin_time_invariant_2sex_cod.R")
 
 
 ## =============================================================================
@@ -297,6 +297,9 @@ timevarying_kin_2sex_cod<- function(Ut, Ft, Ft_star, pit, sex_focal, ages, pkin)
   om <- ages-1
   pif <- pit[1:ages]
   pim <- pit[(ages+1):agess]
+
+  # BEN : Add the number of CoD
+  causes <- nrow(Hf[[1]])
 
   # BEN: Changed dimensions of lower part (dead kin) to account for death from causes.
   phi = d = gd = ggd = m = gm = ggm = os = ys = nos = nys = oa = ya = coa = cya = matrix(0, (agess + agess*causes), ages)
